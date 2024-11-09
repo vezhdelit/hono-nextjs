344badb9e99c523fb190667631bda01c427059d3
Vezhdel Vasyl <vezhdelit@gmail.com>
2024-11-10 00:54:10 +0200

    fix: ts fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,3 @@
-import axios from "axios";
-import { Suspense } from "react";
-
-const getMessage = async () => {
-  const result = await axios.get(process.env.BASE_URL + "/api/hello");
-  return result.data;
-};
-
 export default async function Home() {
-  const message = await getMessage();
-  return (
-    <div className="">
-      <Suspense fallback={<div>Loading...</div>}>
-        <h1>{message.message}</h1>
-      </Suspense>
-    </div>
-  );
+  return <div className=""></div>;
 }
